185ae981e07ad9ad36e8599e36930a5a613287d6
Deepakbst1 <sarassp129@gmail.com>
2025-06-15 10:18:23 +0530

    Merge Sort

--- src/MaximumSubArrayKadane.java
+++ src/MaximumSubArrayKadane.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class MaximumSubArrayKadane {
+import java.util.Scanner;
+
+public class MaximumSubArrayKadane {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size= sc.nextInt();
+        int arr[]=new int[size];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= sc.nextInt();
+        }
+        int result=maximumSub(arr);
+        System.out.println("Maximum Subarray Sum is: " + result);
+    }
+
+    public static int maximumSub(int[] arr) {
+        int currSum=0;
+        int maxSum=Integer.MIN_VALUE;
+        for(int val:arr){
+            currSum+=val;
+            maxSum=Math.max(currSum,maxSum);
+
+            if(currSum<0){
+                currSum=0;
+            }
+        }
+        return  maxSum;
+    }
 }
